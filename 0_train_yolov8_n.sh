3d0e52a84b89c6f6c299521420986db684203f1f
nemonameless <nemonameless@qq.com>
2023-04-24 17:25:43 +0000

    raw codes for yolov5u

---
+++ 0_train_yolov8_n.sh
@@ -0,0 +1,17 @@
+export FLAGS_allocator_strategy=auto_growth
+model_type=yolov8
+job_name=yolov8_n_500e_coco
+# job_name=yolov8_s_500e_coco
+
+config=configs/${model_type}/${job_name}.yml
+log_dir=log_dir/${job_name}
+weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
+
+# 1. training
+#CUDA_VISIBLE_DEVICES=2 python3.7 tools/train.py -c ${config} --eval #-r ${weights} --eval
+#python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3,4,5,6,7 tools/train.py -c ${config} --eval #-r ${weights}
+python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3 tools/train.py -c ${config} --eval #-r ${weights}
+
+# 2. eval
+#CUDA_VISIBLE_DEVICES=0 python3.7 tools/eval.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
+#CUDA_VISIBLE_DEVICES=2 python3.7 tools/eval.py -c ${config} -o weights=${weights}
