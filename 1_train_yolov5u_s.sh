e3e7e0fa1138d2830510b57fc2456b64d3f1ca9c
nemonameless <nemonameless@qq.com>
2023-04-25 04:09:31 +0000

    add clip_grad_by_value

--- 1_train_yolov5u_s.sh
+++ 1_train_yolov5u_s.sh
@@ -1,6 +1,7 @@
 export FLAGS_allocator_strategy=auto_growth
 model_type=yolov5u
 job_name=yolov5u_s_300e_coco
+job_name=yolov5u_n_300e_coco
 # job_name=yolov5u_s_60e_voc
 # job_name=yolov5u_l_60e_voc
 
